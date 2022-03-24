87415acc07a756be619bc240413ed1ba0ac7789a
init.defaultBranch <benlimpic@gmail.com>
2022-03-24 10:15:44 -0700

    commit tree 1dbbac7451f85fb7629059c95b7f213ee626afc4

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+const url = 'https://anapioficeandfire.com/api/books'
+
+const fetchBooks = () => {
+  fetch(url)
+    .then((res) => res.json())
+  .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const main = document.querySelector('main')
+  books.forEach((book) => {
+    const h2 = document.createElement('h2')
+    h2.textContent = book.name
+    main.appendChild(h2)
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// })
